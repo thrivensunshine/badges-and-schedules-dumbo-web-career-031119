3e56f71669cf7fc654c127e841bc6481c4e2a95f
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 02:17:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+  def badge_maker(name)
+  return "Hello, my name is #{name}."
+  end
+
+  def batch_badge_creator(arr)
+  badge_arr = []
+  arr.each do |i|
+  badge_arr << badge_maker(i)
+  end
+  badge_arr
+  end
+  
+  #playing with { } .. didnt work
+  
+  # def batch_badge_creator(arr)
+  # badge_arr = []
+  # arr.each do{|i| badge_arr << badge_maker(i)}
+  # end
+  # badge_arr
+  # end
+  
+def assign_rooms(arr)
+test = []
+arr.each do |i|
+
+test << "Hello, #{i}! You'll be assigned to room #{(arr.index(i)+ 1 )}!"
+end
+test
+end
+
+
+
+def printer(arr)
+
+batch_badge_creator(arr).each {|i| puts i}
+assign_rooms(arr).each {|j| puts j}
+end
